0a68cb4077c7b9f62adc9cd465ec4f6f9d61c28a
marclou <marc.louvion@gmail.com>
2018-03-26 16:36:46 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicPath = path.join(__dirname, '..', '/public');
+const port = process.env.NODE_ENV || '3000';
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Listen on port '+port);
+});
