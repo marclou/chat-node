300ee7ff1beac4c295d2593cc3f0f720da8f1b1f
marclou <marc.louvion@gmail.com>
2018-03-26 17:29:01 +0900

    connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@
 
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', '/public');
 const port = process.env.PORT || '3000';
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Listen on port '+port);
 });
