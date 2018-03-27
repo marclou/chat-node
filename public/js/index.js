baf128ee03e73d47b6eb1718c775977b25e9ee02
marclou <marc.louvion@gmail.com>
2018-03-27 12:41:35 +0900

    adding UI for socket event

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,21 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('New Message : ', message);
+    const { from, text } = message;
+    const li = jQuery('<li></li>');
+
+    li.text(`${from} : ${text}`);
+
+    jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val(),
+        createdAt: new Date().getTime()
+    });
 });
